7e5d7594cb001f327bda9ae615cd2fcb5f3b1cba
little-blackcat <little-blackcat@o2.pl>
2016-11-23 20:54:54 +0000

    new Driver, new Flask runner, add fullScreen to GUI

--- Common/Driver.py
+++ Common/Driver.py
@@ -1,41 +1,74 @@
 import time
 from w1thermsensor import W1ThermSensor
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 
 from DatabaseManager import DatabaseManager
 from Heater import Heater
 
+class Driver:
+    def __init__(self):
+        self.scheduler = BackgroundScheduler()
+        self.db = DatabaseManager('/home/pi/sfc/Common/database.db')
+        self.sensor = W1ThermSensor()
+        self.heater = Heater()
 
-def logTemperature():
-  db.insertTemperature(sensor.get_temperature())
+        self.checkFrequency()
+        self.checkMinMaxTemperature()
 
+        self.scheduler.start()
+        self.initSchedulers()
 
-def heaterController():
-  avg = db.selectAverageLastFive()
-  if avg < 22:
-    heater.on()
-  elif avg > 27:
-    heater.off()
+    def initSchedulers(self):
+        self.lt = self.scheduler.add_job(self.logTemperature, 'interval', seconds=self.frequency)
+        self.uc = self.scheduler.add_job(self.updateConditional, 'interval', seconds=self.frequency)
+        self.ch = self.scheduler.add_job(self.controlHeater, 'interval', seconds=self.frequency)
 
+    def run(self):
+        print('*** Press CTRL-C to exit. ***')
+
+        try:
+            while True:
+                time.sleep(3)
+
+        except(KeyboardInterrupt, SystemExit):
+            print('*** Bye, bye. ***')
+            self.scheduler.shutdown()
+
+    def logTemperature(self):
+        self.db.insertTemperature(self.sensor.get_temperature())
+        
+    def checkFrequency(self):
+        self.frequency = self.db.selectFrequencyFromConf()
+
+    def checkMinMaxTemperature(self):
+        [self.minTemperature, self.maxTemperature] = self.db.selectMinAndMaxTempFromConf()
+
+    def updateConditional(self):
+        self.checkMinMaxTemperature()
+       
+        f = self.db.selectFrequencyFromConf()
+        
+        if(self.frequency != f):
+            self.frequency = f
+
+            self.lt.remove()
+            self.lt = self.scheduler.add_job(self.logTemperature, 'interval', seconds=self.frequency)
+
+            self.uc.remove()
+            self.uc = self.scheduler.add_job(self.updateConditional, 'interval', seconds=self.frequency)
+
+            self.ch.remove()
+            self.ch = self.scheduler.add_job(self.controlHeater, 'interval', seconds=self.frequency)
+
+    def controlHeater(self):
+        avg = self.db.selectAverageLastFive()
+
+        if avg < self.minTemperature:
+            self.heater.on()
+        elif avg > self.maxTemperature:
+            self.heater.off()
 
 if __name__ == '__main__':
-  scheduler = BackgroundScheduler()
-
-  db = DatabaseManager('test.db')
-  sensor = W1ThermSensor()
-  heater = Heater()
-
-  scheduler.add_job(logTemperature, 'interval', seconds=5)
-  scheduler.add_job(heaterController, 'interval', seconds=5)
-
-  scheduler.start()
-
-  print('*** Press Ctrl-C to exit. ***')
-
-  try:
-    while True:
-      time.sleep(3)
-
-  except(KeyboardInterrupt, SystemExit):
-    print('*** Bye, Bye ***')
-    scheduler.shutdown()
+    d = Driver()
+    d.run()
